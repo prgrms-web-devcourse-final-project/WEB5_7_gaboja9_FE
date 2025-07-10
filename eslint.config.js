00f99bca4092137f239663079027efeabf63bd65
joyk231220 <joyk231220@gmail.com>
2025-07-11 07:31:51 +0900

    feat: eslint 설정 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,72 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  {
+    ignores: ['dist/'],
+  },
+
+  js.configs.recommended,
+
   {
     files: ['**/*.{js,jsx}'],
-    plugins: ['import'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-      'eslint-config-prettier',
-    ],
+    plugins: {
+      react: react,
+      import: eslintPluginImport,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': 'warn',
+
+      'no-var': 'error',
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+
       'import/order': [
         'error',
         {
-          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'object', 'type'],
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index', 'object', 'type'],
           pathGroups: [
             {
-              pattern: 'react',
+              pattern: '{react,react-dom/**}',
               group: 'external',
               position: 'before',
             },
           ],
-          pathGroupsExcludedImportTypes: ['builtin'],
           'newlines-between': 'always',
-          alphabetize: { order: 'asc', caseInsensitive: true },
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
         },
       ],
     },
+    settings: {
+      react: {
+        version: 'detect',
+        jsxRuntime: 'automatic',
+      },
+    },
   },
-]);
+
+  eslintConfigPrettier,
+];
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
